019d101849552b851b198af2b02e3e96836f6dbc
陈育宏 <215272371@qq.com>
2019-03-20 17:55:30 +0800

    1.初始化项目

---
+++ eureka-consumer/src/main/java/com/lemon/controller/SubstitutionController.java
@@ -0,0 +1,54 @@
+package com.lemon.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/substitution")
+public class SubstitutionController {
+    @Autowired
+    @Qualifier("remoteRestTemplate")
+    private RestTemplate restTemplate;
+
+//    @Autowired
+//    HelloServiceClient helloServiceClient;
+
+//    @GetMapping("/hello")
+//    @HystrixCommand(fallbackMethod = "defaultHello",
+//            commandProperties = {@HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "1000")})
+//    public String callHello() {
+//        System.out.println(helloServiceClient.hello());
+//        //restTemplate.getForObject("http://LEMON-PROVIDER/helloWorld", String.class)
+//        return helloServiceClient.hello();
+//    }
+//
+//    public String defaultHello() {
+//        return "fail";
+//    }
+
+
+//    @GetMapping("/hello2")
+//    public String callHello2() {
+//        System.out.println(helloServiceClient.hello());
+//        //restTemplate.getForObject("http://LEMON-PROVIDER/helloWorld", String.class)
+//        return helloServiceClient.hello();
+//    }
+
+//    @GetMapping("/hello3")
+//    public String callHello3() {
+//        EurekaConf conf = new EurekaConf();
+//        return conf.getDefaultZone();
+//    }
+
+    @GetMapping("/hello")
+    public String callHello() {
+        //System.out.println(helloServiceClient.hello());
+        //这种方式需要引入ribbon,注解@LoadBalanced
+        String forObject = restTemplate.getForObject("http://LEMON-PROVIDER/helloWorld", String.class);
+        return forObject;
+    }
+}
